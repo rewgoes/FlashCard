9dc62b2dc08b2f0e65d0efcf2821b8e8cb35c688
Rafael Wolf de Goes <rewgoes@gmail.com>
2018-08-29 23:44:52 -0300

    feat: start implementing stack navigator routes

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import { View } from 'react-native';
-import AddDeck from './components/AddDeck';
-import DeckList from './components/DeckList';
+import React from 'react'
+import { View } from 'react-native'
+import { createStackNavigator } from 'react-navigation'
+import DeckList from './components/DeckList'
+
+const MainNavigator = createStackNavigator({
+  Home: {
+    screen: DeckList,
+    navigationOptions: {
+      title: "Flashcards",
+      headerTintColor: 'white',
+      headerStyle: {
+        backgroundColor: 'purple',
+      },
+    }
+  },
+})
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <DeckList />
+        <MainNavigator />
       </View>
-    );
+    )
   }
 }
