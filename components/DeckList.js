3ef0465e39fad15d55835b12cedeea91ab994686
Rafael Wolf de Goes <rewgoes@gmail.com>
2018-09-13 11:39:48 -0300

    feat: style deck list

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, TouchableOpacity, Platform, StyleSheet } from 'react-native'
 import { getDecks } from '../utils/api'
 import { connect } from 'react-redux'
 import { receiveDecks } from '../actions'
@@ -29,21 +29,53 @@ class DeckList extends Component {
     }
 
     if (isEmpty(decks)) {
-      return (<View style={{flex: 1}}/>)
+      return (<View style={{ flex: 1 }} />)
     }
-    
+
     return (
       <View style={{ flex: 1 }}>
         <FlatList
           data={deckList}
           renderItem={({ item }) =>
-            <Text>{item.title}</Text>
+            <TouchableOpacity>
+              <View style={styles.item} >
+                <Text style={styles.itemTitle}>{item.title}</Text>
+                <Text>
+                  {item.questions.length} {item.questions.length !== 1
+                    ? "questions"
+                    : "question"}
+                </Text>
+              </View>
+            </TouchableOpacity>
           }
         />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: 'white',
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    alignItems: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+  },
+  itemTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  }
+})
 
 function mapStateToProps(decks) {
   return {
